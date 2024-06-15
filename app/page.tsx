8a6e83c0094478f24adf807d297fa86b8679b6ef
IPYURA <0207ehdrjs@naver.com>
2024-06-15 16:17:05 +0900

    0.2

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
-import { WritingArea } from './components/WritingArea/WritingArea';
+// import { WritingArea } from './components/WritingArea/WritingArea';
+import Slider from "./components/Slider/Slider";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <WritingArea/>
+      {/* <WritingArea/> */}
+      <Slider projectName="My Project"/>
     </main>
   );
 }
