aa3e8f440420136d35f18473f86d4fa2bd24eb2d
IPYURA <0207ehdrjs@naver.com>
2024-08-14 21:13:38 +0900

    Slider 기능완성

--- components/Projects/Slider.tsx
+++ components/Projects/Slider.tsx
@@ -6,46 +6,57 @@ import { StaticImageData } from "next/image";
 import Slide from "./Slide";
 
 const Slider = ({ images }: { images: StaticImageData[] }) => {
-  const [nowIndex, setNowIndex] = useState(0);
-  const [slideArr, setSlideArr] = useState<StaticImageData[]>(images);
+  const [nowIndex, setNowIndex] = useState(1);
+  const [maxIndex, setMaxIndex] = useState<number>(images.length + 1);
 
-  const initArray = () => {
-    const lastSlide = images[images.length - 1];
-    const firstSlide = images[0];
-    // images.unshift(lastSlide);
-    // images.push(firstSlide);
-    // setSlideArr((arr) => arr.push(lastSlide));
+  const checkLimit = () => {
+    console.log(nowIndex);
+
+    if (nowIndex > maxIndex - 1) {
+      setNowIndex(1);
+      console.log("1로 세팅");
+    }
+    if (nowIndex === 0) {
+      setNowIndex(maxIndex - 1);
+      console.log("maxIndex - 1로 세팅");
+    }
   };
-
-  const moveNext = () => {
-    setNowIndex(nowIndex + 1);
-  };
-  //   const checkLimit = (limit:number) => {
-
-  //   }
 
   useEffect(() => {
-    setSlideArr(images);
-    initArray();
-    // console.log("slideArr의 정체: ", slideArr);
-  }, []);
+    checkLimit();
+  }, [nowIndex]);
 
   return (
     <div className={styles.slider}>
-      <button className={styles.prev}>이전</button>
-      <button onClick={moveNext} className={styles.next}>
-        다음
-      </button>
+      {images.length > 1 ? (
+        <>
+          <button
+            onClick={() => setNowIndex(nowIndex - 1)}
+            className={styles.prev}
+          >
+            이전
+          </button>
+          <button
+            onClick={() => setNowIndex(nowIndex + 1)}
+            className={styles.next}
+          >
+            다음
+          </button>
+        </>
+      ) : undefined}
+
       <div
         className={styles.slideFrame}
         style={{
-          width: `${100 * images.length}%`,
+          width: `${100 * images.length + 200}%`,
           left: `${nowIndex * -100}%`,
         }}
       >
+        <Slide image={images[images.length - 1]} />
         {images?.map((img, index) => (
           <Slide image={img} key={index} />
         ))}
+        <Slide image={images[0]} />
       </div>
     </div>
   );
