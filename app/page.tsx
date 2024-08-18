f69b94c5d0af41504ad18bccc0fe0d9a65ae84af
IPYURA <0207ehdrjs@naver.com>
2024-08-18 23:27:23 +0900

    중간 커밋

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,21 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <h3>테스트 텍스트입니다</h3>
-      </div>      
+      <div className={styles.flexBox}>
+        <div className={styles.picture}>이미지</div>
+        <section className={styles.desc}>
+          <h1 className={styles.title}>
+            <em>Front-End</em> 개발자를 꿈꾸는
+            <br /> 조동건입니다.
+          </h1>
+          <h4 className={styles.introText}>
+            저는 전자공학과에서 C언어를 배우며 프로그래밍에 흥미를 느꼈습니다.
+            앞에 놓인 기계를 움직이는 것을 넘어, 프로그래밍을 통해 웹이라는 더
+            넓은 세상에 영향을 끼치고 싶습니다.
+          </h4>
+          <div className={styles.stackBox}></div>
+        </section>
+      </div>
     </main>
   );
 }
---
+++ components/Home/Stack.tsx
@@ -0,0 +1,16 @@
+import { StaticImageData } from "next/image";
+import React from "react";
+// import { Image } from 'next/image';
+import styles from "@/styles/home.module.scss";
+
+interface IStackProps {
+  name: string;
+  image: StaticImageData;
+}
+const Stack = ({ name, image }: IStackProps) => {
+  return <div>
+    
+  </div>;
+};
+
+export default Stack;
