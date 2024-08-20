e59f32ada789864118e4797d13c9400053340ddb
IPYURA <0207ehdrjs@naver.com>
2024-08-20 15:00:08 +0900

    404page 완성

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,17 @@
+import styles from "@/styles/not-found.module.scss";
+import Image from "next/image";
+import NotFoundImg from "@/public/images/404_image.png";
+import { roboto } from "./font";
+
 const Custom404 = () => {
   return (
     <>
-      <div>
-        <h3>404 페이지입니다.</h3>
+      <div className={styles.notFoundBG}>
+        <div className={styles.contentBox}>
+          <Image src={NotFoundImg} alt="" />
+          <h3 className={roboto.className}>Page Not Found.</h3>
+          <h5>존재하지 않는 페이지입니다.</h5>
+        </div>
       </div>
     </>
   );
